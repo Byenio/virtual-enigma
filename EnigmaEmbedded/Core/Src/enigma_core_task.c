897c5ffe46eefe263d03101cd7d99a3d6e04a4a3
Byenio <marcel.bienko@racing.polsl.pl>
2026-02-22 22:02:23 +0100

    feat(embedded): add display connection and encode button handlers

--- EnigmaEmbedded/Core/Src/enigma_core_task.c
+++ EnigmaEmbedded/Core/Src/enigma_core_task.c
@@ -9,6 +9,54 @@
 static EnigmaMachine enigma;
 
 extern UART_HandleTypeDef huart2;
+extern UART_HandleTypeDef huart1;
+
+#define NEXTION_RX_BUFFER_SIZE 128
+
+uint8_t nextion_rx_byte;
+char nextion_buffer[NEXTION_RX_BUFFER_SIZE];
+volatile uint8_t nextion_idx = 0;
+volatile uint8_t msg_received_flag = 0;
+
+osMessageQueueId_t inputQueueHandle;
+
+void Nextion_SendCommand(const char* cmd)
+{
+  HAL_UART_Transmit(&huart1, (uint8_t*)cmd, strlen(cmd), 100);
+  const uint8_t terminator[] = { 0xFF, 0xFF, 0xFF };
+  HAL_UART_Transmit(&huart1, terminator, 3, 100);
+}
+
+void Nextion_SetText(char* object_name, char* text)
+{
+  char cmd[100];
+  snprintf(cmd, sizeof(cmd), "%s.txt=\"%s\"", object_name, text);
+  Nextion_SendCommand(cmd);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  if (huart->Instance == USART1)
+  {
+    if (nextion_rx_byte == '#' || nextion_idx >= NEXTION_RX_BUFFER_SIZE - 1)
+    {
+      nextion_buffer[nextion_idx] = '\0';
+
+      if (inputQueueHandle != NULL)
+      {
+        osMessageQueuePut(inputQueueHandle, nextion_buffer, 0, 0);
+      }
+
+      nextion_idx = 0;
+    }
+    else
+    {
+      nextion_buffer[nextion_idx++] = nextion_rx_byte;
+    }
+
+    HAL_UART_Receive_IT(&huart1, &nextion_rx_byte, 1);
+  }
+}
 
 static void UART_SendString(const char* str)
 {
@@ -41,21 +89,44 @@ void EnigmaSetup(void)
   RotorInit(&temp_rotor, kEnigmaRotor_III_Wiring, kEnigmaRotor_III_Notch, 0);
   RotorSetPosition(&temp_rotor, 'A');
   EnigmaAddRotor(&enigma, &temp_rotor);
+
+  inputQueueHandle = osMessageQueueNew(5, 64, NULL);
+  HAL_UART_Receive_IT(&huart1, &nextion_rx_byte, 1);
 }
 
 void EnigmaTask(void *arg)
 {
-  char text_buffer[64] = "RTOS RUNNING";
-  char output_buffer[128];
+  char input_buffer[64];
+  char output_buffer[64];
+  char debug_msg[64];
 
-  char encrypted[64];
-  strcpy(encrypted, text_buffer);
+  // char encrypted[64];
+  // strcpy(encrypted, text_buffer);
+  //
+  // EnigmaEncryptString(&enigma, encrypted);
+  //
+  // snprintf(output_buffer, sizeof(output_buffer), "%s -> %s", text_buffer, encrypted);
+  //
+  // UART_SendString(output_buffer);
+  //
+  // osDelay(1000);
 
-  EnigmaEncryptString(&enigma, encrypted);
+  for (;;)
+  {
+    osStatus_t status = osMessageQueueGet(inputQueueHandle, input_buffer, NULL, osWaitForever);
 
-  snprintf(output_buffer, sizeof(output_buffer), "%s -> %s", text_buffer, encrypted);
+    if (status == osOK)
+    {
+      snprintf(debug_msg, sizeof(debug_msg), "[RX] %s", input_buffer);
+      UART_SendString(debug_msg);
 
-  UART_SendString(output_buffer);
+      strcpy(output_buffer, input_buffer);
+      EnigmaEncryptString(&enigma, output_buffer);
 
-  osDelay(1000);
+      Nextion_SetText("output_box", output_buffer);
+
+      snprintf(debug_msg, sizeof(debug_msg), "[OUT] %s", output_buffer);
+      UART_SendString(debug_msg);
+    }
+  }
 }
--- EnigmaEmbedded/Core/Src/main.c
+++ EnigmaEmbedded/Core/Src/main.c
@@ -41,6 +41,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* Definitions for enigmaCoreTask */
@@ -58,6 +59,7 @@ const osThreadAttr_t enigmaCoreTask_attributes = {
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 void StartEnigmaCoreTask(void *argument);
 
 /* USER CODE BEGIN PFP */
@@ -99,6 +101,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -200,6 +203,41 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -288,12 +326,8 @@ void StartEnigmaCoreTask(void *argument)
 {
   /* USER CODE BEGIN 5 */
   EnigmaSetup();
-  /* Infinite loop */
-  for(;;)
-  {
-    EnigmaTask(argument);
-    osDelay(1000);
-  }
+  EnigmaTask(argument);
+  osThreadTerminate(NULL);
   /* USER CODE END 5 */
 }
 
